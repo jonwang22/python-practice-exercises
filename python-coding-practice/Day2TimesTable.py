ca2ba8db73f7b823e2015eb0333e3085c855617e
Jon Wang <jonwang.kuralabs@gmail.com>
2024-10-22 04:25:59 +0000

    adding logic

--- python-coding-practice/Day2TimesTable.py
+++ python-coding-practice/Day2TimesTable.py
@@ -13,3 +13,10 @@
 # 8 * 2 = 16
 # 9 * 2 = 18
 
+# List of first 10 numbers
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# iterate through list and multiply number by 2
+for number in list:
+    product = number * 2
+    print(f"{number} * 2 = {product}")
