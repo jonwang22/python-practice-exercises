732813e37b9be6bbea4295f9e32dffd793bb4752
Jon Wang <jonwang.kuralabs@gmail.com>
2024-10-22 20:23:02 +0000

    first attempt

--- python-coding-practice/Day6DictionariesDataParsing.py
+++ python-coding-practice/Day6DictionariesDataParsing.py
@@ -3,3 +3,28 @@
 # Output Example:
 
 # Name: John, Age: 20, Grade: A Name: Jane, Age: 22, Grade: B Name: Alice, Age: 19, Grade: A
+
+# Creating Lists for name, age, grade
+name = ["John", "Jane", "Alice"]
+age = [20, 22, 19]
+grade = ["A", "B", "A"]
+
+# Creating dictionary
+dict = {
+    "Name": name,
+    "Age": age,
+    "Grade": grade,
+}
+
+length = len(dict)
+empty = ""
+
+for i in range(length):
+    for key, value in dict.items():
+        if i == length-1:
+            var = f"{key.title()}: {value[i]} "
+        else:
+            var = f"{key.title()}: {value[i]}, "
+        empty += var
+
+print(empty)
