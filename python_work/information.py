f13ca18b2fc5c51f263669d0ac2abffc127522b5
Jon <jonwang.kuralabs@gmail.com>
2024-11-24 05:54:28 +0000

    adding missing files

--- python_work/information.py
+++ python_work/information.py
@@ -3,3 +3,43 @@ print(name)
 
 secondName = "Bruce"
 print(name + " " + secondName)
+
+weight = "300"
+print("My name is " + secondName + " " + "and I weigh " + weight + ".")
+
+height = "6ft 3in"
+print(height)
+
+# We can use commas to concatenate variables and strings
+print(name,height,weight)
+
+bloodPressure = "120/80"
+print(bloodPressure)
+
+print(type(bloodPressure))
+
+temperature = 98.5
+print(temperature)
+print(type(temperature))
+
+superPowers = ["strength", "intelligence", "stealthy", "wealth"]
+print(superPowers)
+print(type(superPowers))
+print(len(superPowers))
+
+superPowers = ("strength","intelligence","stealthy","wealth")
+print(type(superPowers))
+
+originalWeight = "300"
+weightGained = "20"
+print(originalWeight + weightGained)
+
+originalWeight = 300
+weightGained = 20
+print(originalWeight + weightGained)
+
+temperature = int(96.3)
+print(temperature)
+print(type(temperature))
+
+print(f"My name is {name} and I weight {weight} pounds.")
---
+++ python_work/numbers.py
@@ -0,0 +1,46 @@
+# x = 5
+# y = 3
+# sum = x + y
+# print(sum)
+# print(type(sum))
+
+# product = x * y
+# print(product)
+# print(type(product))
+
+# diff = y - x
+# print(diff)
+# print(type(diff))
+
+# x = 9
+# y = 3
+# quotient = x / y
+# print(quotient)
+# print(type(quotient))
+
+
+# x = 10
+# y = 3
+# modulo = x % y
+# print(modulo)
+# print(type(modulo))
+
+superHero = {
+        "Batman": {"height":"6ft 3in", "weight":300},
+        "Superman": {"height":"6ft 6in","weight":280},
+        "favorite foods": ("shwarma","pizza","pasta"),
+}
+print(superHero["Batman"])
+print(superHero["Batman"]["weight"])
+print(type(superHero))
+print(type(superHero["Batman"]))
+
+print(superHero["favorite foods"][0])
+
+
+# Dictionary: Key-Value pairs
+super_hero_names = ["batman", "deadpool", "wolverine"]
+print("super_hero_names", super_hero_names)
+print(super_hero_names[0])
+
+
